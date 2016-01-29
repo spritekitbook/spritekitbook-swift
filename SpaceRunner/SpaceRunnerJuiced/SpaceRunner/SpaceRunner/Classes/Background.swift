12b85170d3381c5e5c2776d48fd6e1977094f1f4
Jeremy Novak <jgnovak@gmail.com>
2016-01-28 21:55:03 -0600

    Project Organization
    Project Organization

--- SpaceRunner/SpaceRunnerJuiced/SpaceRunner/SpaceRunner/Classes/Background.swift
+++ SpaceRunner/SpaceRunnerJuiced/SpaceRunner/SpaceRunner/Classes/Background.swift
@@ -16,6 +16,7 @@ class Background:SKNode {
     
     // MARK: - Private class variables
     private var backgroundParticles = SKEmitterNode()
+    private var backgroundParticlesSmall = SKEmitterNode()
     
     // MARK: - Init
     required init?(coder aDecoder: NSCoder) {
@@ -32,7 +33,12 @@ class Background:SKNode {
     private func setupBackground() {
         self.backgroundParticles = GameParticles.sharedInstance.createParticle(particles: GameParticles.Particles.Magic)
         
+        // Small white particles
+        self.backgroundParticlesSmall = GameParticles.sharedInstance.createParticle(particles: GameParticles.Particles.Magic)
+        self.backgroundParticlesSmall.particleScale = 0.25
+        
         self.addChild(self.backgroundParticles)
+        self.addChild(self.backgroundParticlesSmall)
         
         self.stopBackground()
     }
@@ -41,10 +47,16 @@ class Background:SKNode {
     func startBackground() {
         self.backgroundParticles.particleSpeed = self.backgroundRunSpeed
         self.backgroundParticles.particleSpeedRange = self.backgroundRunSpeed / 4
+        
+        self.backgroundParticlesSmall.particleSpeed = self.backgroundRunSpeed * 1.5
+        self.backgroundParticlesSmall.particleSpeedRange = self.backgroundParticlesSmall.particleSpeed / 4
     }
     
     func stopBackground() {
         self.backgroundParticles.particleSpeed = self.backgroundStopSpeed
         self.backgroundParticles.particleSpeedRange = self.backgroundStopSpeed / 4
+        
+        self.backgroundParticlesSmall.particleSpeed = self.backgroundStopSpeed * 1.5
+        self.backgroundParticlesSmall.particleSpeedRange = self.backgroundParticlesSmall.particleSpeed / 4
     }
 }
